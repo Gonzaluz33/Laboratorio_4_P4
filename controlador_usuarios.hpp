6ceb17993eef3da33c90ef00d83fead3275f8209
Gonzaluz33 <gonzaluz2011@gmail.com>
2023-05-29 15:24:13 -0300

    termine la clase COntrolador Usuarios

--- controlador_usuarios.hpp
+++ controlador_usuarios.hpp
@@ -1,7 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Idioma;
+class DTEstEstudiante;
+class DTEstProfesor;
+class Curso;
+class Notificacion;
+
 class ControladorUsuarios {
     private: 
         static ControladorUsuarios * instancia;
         ControladorUsuarios();
     public:
     static ControladorUsuarios * getInstance();
+    void iniciarAltaUsuario (string nickname ,string contraseña,string nombre,string descripción);
+    void datosAdicionalesEstudiante(string nom_Pais,DTFecha fecha_Nacimiento);
+    void datosAdicionalesProfesor(string nom_Instituto);
+    vector <Idioma*> listaIdiomas();
+    void seleccionarIdioma(string nom_Idioma);
+    bool altaUsuario();
+    vector <string> listarEstudiantes();
+    vector <DTEstEstudiante*> listarEstEstudiante(string nickname);
+    vector <string> listarProfesores();
+    vector <DTEstProfesor*> listarEstProfesor(string nickname);
+    vector <Idioma*> listaIdiomasProfesor(string nickname);
+    vector <Curso*> listarCursosNoAprobados(string nickname);
+    vector <Notificacion*> listarNotificaciones(string nickname);
+    vector <Idioma*>listarIdiomasSuscritos(string nickname);
+    vector <Idioma*> listarIdiomasNoSuscritos(string nickname);
+    void suscribir(vector <Idioma*> idiomasASuscribir);
+    void eliminarSuscripcion(vector <Idioma*>idiomasAEliminar);
 };
