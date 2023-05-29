5f788f1d2ddf7590286c1b90fc783680b07495a4
Gonzaluz33 <gonzaluz2011@gmail.com>
2023-05-29 15:36:49 -0300

    arreglo error

--- controlador_usuarios.hpp
+++ controlador_usuarios.hpp
@@ -2,11 +2,11 @@
 #include <vector>
 using namespace std;
 
-class Idioma;
+class DTIdioma;
 class DTEstEstudiante;
 class DTEstProfesor;
-class Curso;
-class Notificacion;
+class DTCurso;
+class DTNotificacion;
 
 class ControladorUsuarios {
     private: 
@@ -17,18 +17,17 @@ class ControladorUsuarios {
     void iniciarAltaUsuario (string nickname ,string contraseña,string nombre,string descripción);
     void datosAdicionalesEstudiante(string nom_Pais,DTFecha fecha_Nacimiento);
     void datosAdicionalesProfesor(string nom_Instituto);
-    vector <Idioma*> listaIdiomas();
+    vector <DTIdioma*> listaIdiomas();
     void seleccionarIdioma(string nom_Idioma);
     bool altaUsuario();
     vector <string> listarEstudiantes();
     vector <DTEstEstudiante*> listarEstEstudiante(string nickname);
     vector <string> listarProfesores();
     vector <DTEstProfesor*> listarEstProfesor(string nickname);
-    vector <Idioma*> listaIdiomasProfesor(string nickname);
-    vector <Curso*> listarCursosNoAprobados(string nickname);
-    vector <Notificacion*> listarNotificaciones(string nickname);
-    vector <Idioma*>listarIdiomasSuscritos(string nickname);
-    vector <Idioma*> listarIdiomasNoSuscritos(string nickname);
+    vector <DTIdioma*> listaIdiomasProfesor(string nickname);
+    vector <DTCurso*> listarCursosNoAprobados(string nickname);
+    vector <DTIdioma*>listarIdiomasSuscritos(string nickname);
+    vector <DTIdioma*> listarIdiomasNoSuscritos(string nickname);
     void suscribir(vector <Idioma*> idiomasASuscribir);
     void eliminarSuscripcion(vector <Idioma*>idiomasAEliminar);
 };
