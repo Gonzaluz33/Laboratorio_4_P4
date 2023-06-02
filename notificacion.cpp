bbdf733f54b52528e160a618aac573eff9a149fb
camilomdeoca <71662256+camilomdeoca@users.noreply.github.com>
2023-06-02 11:15:11 -0300

    Update notificacion.cpp

--- notificacion.cpp
+++ notificacion.cpp
@@ -1,6 +1,24 @@
 #include "notificacion.hpp"
 
+Notificacion::Notificacion(string nombre_curso, string nombre_idioma):
+    nombre_curso(nombre_curso),
+    nombre_idioma(nombre_idioma)
+{
+}
+
+Notificacion::~Notificacion() {
+
+}
+
 DTNotificacion Notificacion::getDataNotificacion(){
     DTNotificacion dtNot = DTNotificacion(this->nombre_curso, this->nombre_idioma);
     return dtNot;
+}
+
+string Notificacion::getNombreCurso() {
+    return nombre_curso;
+}
+
+string Notificacion::getNombreIdioma() {
+    return nombre_idioma;
 }
