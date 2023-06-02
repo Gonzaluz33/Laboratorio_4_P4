533191f968d3cec1473c626664c56ab966057484
camilomdeoca <71662256+camilomdeoca@users.noreply.github.com>
2023-06-01 23:01:47 -0300

    implementacion fabrica

--- fabrica.cpp
+++ fabrica.cpp
@@ -6,3 +6,15 @@ Fabrica::Fabrica() {
     controladorCursos = new ControladorCursos();
     controladorUsuarios = ControladorUsuarios::getInstance();
 }
+
+Fabrica::~Fabrica() {
+
+}
+
+IControladorCursos *Fabrica::getIControladorCursos() {
+    return controladorCursos;
+}
+
+IControladorUsuarios *Fabrica::getIControladorUsuarios() {
+    return controladorUsuarios;
+}
