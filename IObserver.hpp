3f246cd77368346ff2e07422a76a8149fde46436
Julieta <juli100ta@gmail.com>
2023-06-02 09:37:39 -0300

    Update IObserver.hpp

--- IObserver.hpp
+++ IObserver.hpp
@@ -2,6 +2,6 @@
 
 class IObserver{
     public:
-        virtual void notificar(string nombre_curso, string nombre_idioma) = 0; //se define en los observadores - va virtual??
-        void eliminarNotificaciones(string nombre_curso);
+        virtual void notificar(string nombre_curso, string nombre_idioma) = 0; //se define en los observadores 
+        virtual void eliminarNotificaciones(string nombre_curso) = 0;
 };
