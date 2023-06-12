6197f55ce75997134125780c91ce88145461db41
Julieta <juli100ta@gmail.com>
2023-06-12 09:25:26 -0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -73,7 +73,7 @@ int main(){
                 cin >> descripcion;
                 int opcion_tipo;
                 do{
-                    cout << "Ingrese el tipo de usuario (1_ Profesor 2_Estudiante): "<< endl;
+                    cout << "Ingrese el tipo de usuario: "<< endl;
                     << "1. Profesor "<<endl<<"2. Estudiante "<< endl;
                     cin >> opcion_tipo;
                     if(opcion_tipo < 1 || opcion_tipo > 2)
